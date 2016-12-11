29458b1856e2a29e94dbe79f67f23a5961d0b20d
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2016-12-11 15:29:17 -0500

    Clearning up recording and moar data

--- utils/record.lua
+++ utils/record.lua
@@ -1,30 +1,34 @@
 package.path = "../lib/?.lua;../config/?.lua;" .. package.path
 
-Mario = require('Mario')
 Interface = require('Interface')
+Mario = require('Mario')
 Config = require('config')
 Fileops = require('Fileops')
 
 
-smb_savestate = savestate.create(0)
+smb_savestate = savestate.create(1)
 getInputs = Mario.getInputs
 displayBoard = Interface.displayBoard
 totalGameState = {}
-run = 0
+run = 1
 Interface.initializeRun(smb_savestate)
 while true do
     displayBoard()
-
 	playerStatus = memory.readbyte(0x000E)
-	if playerStatus ~= 11 and playerStatus ~= 4  then
+    ypos =  memory.readbyte(0x03B8)+16
+    gui.drawtext(223, 88, ypos, color)
+
+	if playerStatus ~= 11 and playerStatus ~= 4  and ypos < 220 then
   		gamestate = displayBoard()
 		table.insert(totalGameState, gamestate)
-    else
-    	Fileops.write_full_game(totalGameState, run .. ".txt")
-    	totalGameState = {}
-    	run = run + 1
+    else 
+        if playerStatus ~= 11 and ypos < 220 then
+        	Fileops.write_full_game(totalGameState, run .. ".txt")
+        	run = run + 1
+        end
+        totalGameState = {}
     	Interface.initializeRun(smb_savestate)
-	end
+    end
 
     emu.frameadvance()
 end
