4a151d570931494973d1ea2ed5cc9aaa2bfc3c16
Nicolas Agostini <n.b.agostini@gmail.com>
2016-12-11 16:19:44 -0500

    Missing datamanip

--- lib/Datamanipulation.lua
+++ lib/Datamanipulation.lua
@@ -1,23 +1,22 @@
 
 local Datamanipulation = {}
 -- ####################################################################
-function Datamanipulation.getResizedVectorLine(inputTensorLine)
+function Datamanipulation.getResizedVectorLine(inputTensorLine,slice)
   local reshaped_file_tensor = {}
   local resized_tensor = {}
   local vectorTensor = {}
     reshaped_file_tensor = torch.DoubleTensor(13,13):copy(inputTensorLine)
     --print(reshaped_file_tensor[i][{{6,10},{6,13}}])
-    local slice = {{6,10},{6,13}}
     resized_tensor = reshaped_file_tensor[slice]
-    vectorTensorLine=torch.DoubleTensor(40):copy(resized_tensor)
+    vectorTensorLine=torch.DoubleTensor(35):copy(resized_tensor)
   return vectorTensorLine
 end
 
 -- ####################################################################
-function Datamanipulation.getResizedVector(inputTensor)
+function Datamanipulation.getResizedVector(inputTensor,slice)
   local vectorTensor = {}
   for i=1,(#inputTensor)[1] do
-    vectorTensor[i]=Datamanipulation.getResizedVectorLine(inputTensor[i])
+    vectorTensor[i]=Datamanipulation.getResizedVectorLine(inputTensor[i],slice)
   end
   return vectorTensor
 end
