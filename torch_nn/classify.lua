f67d582375363dcbef6dcdfd0e9e1c8dfdbd8e3d
Nicolas Agostini <n.b.agostini@gmail.com>
2016-12-10 23:47:44 -0500

    Implementing MultilablelCriteria

--- torch_nn/classify.lua
+++ torch_nn/classify.lua
@@ -2,20 +2,19 @@ torch = require "torch"
 ffi = require 'ffi'
 nn = require "nn"
 
-
 -- ####################################################################
 function get_int_from_bin(line)
-     number = tonumber(line:gsub(" " , ""),2)
-    return number
+  number = tonumber(line:gsub(" " , ""),2)
+  return number
 end
 -- ####################################################################
 function get_data_and_labelsMULTILABEL(dataPath,labelsPath)
-    mySet = {}
-    local data=load_file_to_tensor_with_type(dataPath,"byte")
-    local labels=load_file_to_labels_with_type(labelsPath, "byte")
-	mySet.data = data
-    mySet.label = labels
-    return mySet
+  mySet = {}
+  local data=load_file_to_tensor_with_type(dataPath,"byte")
+  local labels=load_file_to_labels_with_type(labelsPath, "byte")
+  mySet.data = data
+  mySet.label = labels
+  return mySet
 end
 
 -- ####################################################################
@@ -31,17 +30,17 @@ function load_file_to_labels_with_type(path,thetype)
 
   -- intialize tensor for the file
 
-        local file_tensor = {}
-    if thetype == "double" then
-          file_tensor = torch.DoubleTensor(input_table)
+  local file_tensor = {}
+  if thetype == "double" then
+    file_tensor = torch.DoubleTensor(input_table)
 
-    elseif thetype == "byte" then
-          file_tensor = torch.ByteTensor(input_table)
-    elseif thetype == "int" then
-          file_tensor = torch.IntTensor(input_table)
-    else
-        file_tensor = torch.IntTensor(input_table)
-    end
+  elseif thetype == "byte" then
+    file_tensor = torch.ByteTensor(input_table)
+  elseif thetype == "int" then
+    file_tensor = torch.IntTensor(input_table)
+  else
+    file_tensor = torch.IntTensor(input_table)
+  end
 
   return file_tensor
 end
@@ -58,72 +57,151 @@ function load_file_to_tensor_with_type(path,thetype)
   for line in file:lines() do
     input_table[line_number] = {}
     for input in line:gmatch("%w+") do
-            table.insert(input_table[line_number], input)
+      table.insert(input_table[line_number], input)
     end
     -- increment the number of lines counter
     line_number = line_number +1
   end
   file:close() --close file
   -- intialize tensor for the file
-    local file_tensor = {}
-    if thetype == "double" then
-          file_tensor = torch.DoubleTensor(input_table)
+  local file_tensor = {}
+  if thetype == "double" then
+    file_tensor = torch.DoubleTensor(input_table)
 
-    elseif thetype == "byte" then
-          file_tensor = torch.ByteTensor(input_table)
-    elseif thetype == "int" then
-          file_tensor = torch.IntTensor(input_table)
-    else
-        file_tensor = torch.IntTensor(input_table)
-    end
+  elseif thetype == "byte" then
+    file_tensor = torch.ByteTensor(input_table)
+  elseif thetype == "int" then
+    file_tensor = torch.IntTensor(input_table)
+  else
+    file_tensor = torch.IntTensor(input_table)
+  end
 
   return file_tensor
 end
+-- ####################################################################
+function load_file_to_labelsNEW(path)
 
+  local input_table = {}
+  local file = io.open(path, 'r') -- open file
+  for line in file:lines() do
+    local int_table = {}
+    line:gsub(".",function(c) table.insert(int_table,tonumber(c)) end)
+    table.insert(input_table, torch.DoubleTensor(int_table))
+  end
+  file:close() --close file
+
+  -- intialize tensor for the file
+  --local file_tensor = torch.IntTensor(input_table)
+  return input_table
+end
+-- ####################################################################
+-- this function loads a file line by line to avoid having memory issues
+function load_file_to_tensorNEW(path)
+
+  local input_table = {}
+
+  local file = io.open(path, 'r') -- open file
+  local max_line_size = 0
+  local line_number = 1
+  for line in file:lines() do
+    input_table[line_number] = {}
+    for input in line:gmatch("%w+") do
+      table.insert(input_table[line_number], input)
+    end
+    -- increment the number of lines counter
+    line_number = line_number +1
+  end
+  file:close() --close file
+  -- intialize tensor for the file
+  local file_tensor = torch.DoubleTensor(input_table)
+  return file_tensor
+end
+-- ####################################################################
+function get_data_and_labelsNEW(dataPath,labelsPath)
+  mySet = {}
+  local data=load_file_to_tensorNEW(dataPath)
+  function mySet:size() return (#data)[1] end
+  local labels=load_file_to_labelsNEW(labelsPath)
+  for i=1, mySet:size() do
+    mySet[i] = {data[i], labels[i]}
+  end
+  return mySet
+end
 
 -- ----------------------------------------------------------------
--- Load NN objsect
-net = torch.load('nnparame.par')
 
---load data
--- Load the data
-dataPath = "data_1.txt"
-labelsPath = "labels_1.txt"
-dataset={}
-dataset = get_data_and_labelsMULTILABEL(dataPath,labelsPath)
+if false then
+  -- Load NN objsect
+  net = torch.load('nnparame.par')
 
-setmetatable(dataset,
-				{__index = function(t, i)
-				return {t.data[i], t.label[i]}
-				end}
-			);
-		dataset.data = dataset.data:double() -- convert the data from a ByteTensor to
-		-- a DoubleTensor.
+  --load data
+  -- Load the data
+  dataPath = "data_1.txt"
+  labelsPath = "labels_1.txt"
+  dataset={}
+  dataset = get_data_and_labelsMULTILABEL(dataPath,labelsPath)
 
-function dataset:size()
-    return self.data:size(1)
-end
-for i=1, 2993 do
-dataset.label[i]=dataset.label[i]+1
-end
+  setmetatable(dataset,
+    {__index = function(t, i)
+        return {t.data[i], t.label[i]}
+        end}
+    );
+    dataset.data = dataset.data:double() -- convert the data from a ByteTensor to
+    -- a DoubleTensor.
 
-class_performance = {
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0}
-for i=1,dataset:size()  do
-    local groundtruth = dataset.label[i]
-    local prediction = net:forward(dataset.data[i])
-    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
-    if groundtruth == indices[1] then
-        class_performance[groundtruth] = class_performance[groundtruth] + 1
+    function dataset:size()
+      return self.data:size(1)
     end
-end
+    for i=1, 2993 do
+      dataset.label[i]=dataset.label[i]+1
+    end
 
-for i=1, 64 do
-    print(i, class_performance[i]/dataset:size() .. ' %')
-end
+    class_performance = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0}
+    for i=1,dataset:size() do
+      local groundtruth = dataset.label[i]
+      print(dataset.data[i])
+      print(#dataset.data[i])
+      local prediction = net:forward(dataset.data[i])
+      local confidences, indices = torch.sort(prediction, true) -- true means sort in descending order
+      print(indices[1])
+      if groundtruth == indices[1] then
+        class_performance[groundtruth] = class_performance[groundtruth] + 1
+      end
+    end
+
+    for i=1, 64 do
+      print(i, class_performance[i]/dataset:size() .. ' %')
+    end
+
+  end
+
+  if true then
+    -- Load NN objsect
+    net = torch.load('multilabel.par')
+
+    --load data
+    -- Load the data
+    dataPath = "../Data/data_2/data_1.txt"
+    labelsPath = "../Data/data_2/labels_1.txt"
+    dataset={}
+    dataset = get_data_and_labelsNEW(dataPath,labelsPath)
+
+    for i=1,#dataset do
+      prediction = net:forward(dataset[i][1])
+      print(prediction)
+    end
+
+  -- this is the order in which the buttos are on the labels file
+  classes = {'A', 'B', 'Down', 'Left',
+    'Right', 'Up'}
+  --print(classes[dataset[2][2]])
+
+
+  end
--- torch_nn/read_data.lua
+++ torch_nn/read_data.lua
@@ -1,4 +1,3 @@
-
 torch = require "torch"
 ffi = require 'ffi'
 nn = require "nn"
@@ -17,8 +16,8 @@ function load_file_to_tensor(path)
   local line_number = 1
   for line in file:lines() do
     input_table[line_number] = {}
-    for input in line:gmatch("%w+") do 
-            table.insert(input_table[line_number], input) 
+    for input in line:gmatch("%w+") do
+      table.insert(input_table[line_number], input)
     end
     -- increment the number of lines counter
     line_number = line_number +1
@@ -40,8 +39,8 @@ function load_file_to_tensorNEW(path)
   local line_number = 1
   for line in file:lines() do
     input_table[line_number] = {}
-    for input in line:gmatch("%w+") do 
-            table.insert(input_table[line_number], input) 
+    for input in line:gmatch("%w+") do
+      table.insert(input_table[line_number], input)
     end
     -- increment the number of lines counter
     line_number = line_number +1
@@ -63,25 +62,25 @@ function load_file_to_tensor_with_type(path,thetype)
   local line_number = 1
   for line in file:lines() do
     input_table[line_number] = {}
-    for input in line:gmatch("%w+") do 
-            table.insert(input_table[line_number], input) 
+    for input in line:gmatch("%w+") do
+      table.insert(input_table[line_number], input)
     end
     -- increment the number of lines counter
     line_number = line_number +1
   end
   file:close() --close file
   -- intialize tensor for the file
-    local file_tensor = {}
-    if thetype == "double" then
-          file_tensor = torch.DoubleTensor(input_table)
-    
-    elseif thetype == "byte" then
-          file_tensor = torch.ByteTensor(input_table)
-    elseif thetype == "int" then
-          file_tensor = torch.IntTensor(input_table) 
-    else 
-        file_tensor = torch.IntTensor(input_table)
-    end
+  local file_tensor = {}
+  if thetype == "double" then
+    file_tensor = torch.DoubleTensor(input_table)
+
+  elseif thetype == "byte" then
+    file_tensor = torch.ByteTensor(input_table)
+  elseif thetype == "int" then
+    file_tensor = torch.IntTensor(input_table)
+  else
+    file_tensor = torch.IntTensor(input_table)
+  end
 
   return file_tensor
 end
@@ -92,12 +91,12 @@ function load_file_to_labelsNEW(path)
   local input_table = {}
   local file = io.open(path, 'r') -- open file
   for line in file:lines() do
-      local int_table = {}
-      line:gsub(".",function(c) table.insert(int_table,tonumber(c)) end)
-      table.insert(input_table, torch.DoubleTensor(int_table))
+    local int_table = {}
+    line:gsub(".",function(c) table.insert(int_table,tonumber(c)) end)
+    table.insert(input_table, torch.DoubleTensor(int_table))
   end
   file:close() --close file
-    
+
   -- intialize tensor for the file
   --local file_tensor = torch.IntTensor(input_table)
   return input_table
@@ -113,28 +112,28 @@ function load_file_to_labels_with_type(path,thetype)
 
   end
   file:close() --close file
-    
+
   -- intialize tensor for the file
-    
-        local file_tensor = {}
-    if thetype == "double" then
-          file_tensor = torch.DoubleTensor(input_table)
-    
-    elseif thetype == "byte" then
-          file_tensor = torch.ByteTensor(input_table)
-    elseif thetype == "int" then
-          file_tensor = torch.IntTensor(input_table) 
-    else 
-        file_tensor = torch.IntTensor(input_table)
-    end
-    
+
+  local file_tensor = {}
+  if thetype == "double" then
+    file_tensor = torch.DoubleTensor(input_table)
+
+  elseif thetype == "byte" then
+    file_tensor = torch.ByteTensor(input_table)
+  elseif thetype == "int" then
+    file_tensor = torch.IntTensor(input_table)
+  else
+    file_tensor = torch.IntTensor(input_table)
+  end
+
   return file_tensor
 end
 
 -- ####################################################################
 function get_int_from_bin(line)
-     number = tonumber(line:gsub(" " , ""),2)
-    return number
+  number = tonumber(line:gsub(" " , ""),2)
+  return number
 end
 -- ####################################################################
 function load_file_to_labels(path)
@@ -146,7 +145,7 @@ function load_file_to_labels(path)
 
   end
   file:close() --close file
-    
+
   -- intialize tensor for the file
   local file_tensor = torch.IntTensor(input_table)
   return input_table
@@ -154,110 +153,158 @@ end
 
 -- ####################################################################
 function get_data_and_labels(dataPath,labelsPath)
-    mySet = {}
-    data=load_file_to_tensor(dataPath)
-    labels=load_file_to_labels(labelsPath)
-    mySet.data = data
-    mySet.label = labels
-    
-    return mySet
+  mySet = {}
+  data=load_file_to_tensor(dataPath)
+  labels=load_file_to_labels(labelsPath)
+  mySet.data = data
+  mySet.label = labels
+
+  return mySet
 end
 
 -- ####################################################################
 function get_data_and_labelsNEW(dataPath,labelsPath)
-    mySet = {}
-    local data=load_file_to_tensorNEW(dataPath)
-    function mySet:size() return (#data)[1] end
-    local labels=load_file_to_labelsNEW(labelsPath)
-    for i=1, mySet:size() do
-        mySet[i] = {data[i], labels[i]}
-    end
-    return mySet
+  mySet = {}
+  local data=load_file_to_tensorNEW(dataPath)
+  function mySet:size() return (#data)[1] end
+  local labels=load_file_to_labelsNEW(labelsPath)
+  for i=1, mySet:size() do
+    mySet[i] = {data[i], labels[i]}
+  end
+  return mySet
 end
 
 -- ####################################################################
 function get_data_and_labelsMULTILABEL(dataPath,labelsPath)
-    mySet = {}
-    local data=load_file_to_tensor_with_type(dataPath,"byte")
-    local labels=load_file_to_labels_with_type(labelsPath, "byte")
-	mySet.data = data
-    mySet.label = labels
-    return mySet
+  mySet = {}
+  local data=load_file_to_tensor_with_type(dataPath,"byte")
+  local labels=load_file_to_labels_with_type(labelsPath, "byte")
+  mySet.data = data
+  mySet.label = labels
+  return mySet
 end
-
-
 
 -- #####################################################################
-trainthis = false 
-if trainthis then 
-
--- Load the data
-dataPath = "data_1.txt"
-labelsPath = "labels_1.txt"
-dataset={}
-dataset = get_data_and_labelsNEW(dataPath,labelsPath)
-
-mlp = nn.Sequential();  -- make a multi-layer perceptron
-inputs = 169; outputs = 6; HUs = 45; -- parameters
-mlp:add(nn.Linear(inputs, HUs))
-mlp:add(nn.Tanh())
-mlp:add(nn.Linear(HUs, outputs))
-
-print('Lenet5\n' .. mlp:__tostring());
-mlp:zeroGradParameters() -- zero the internal gradient buffers of the network
--- (will come to this later)
-
-criterion = nn.MSECriterion()
-trainer = nn.StochasticGradient(mlp, criterion)
-trainer.learningRate = 0.01
-trainer.maxIteration = 10 -- just do 5 epochs of training.
-trainer:train(dataset)
-
-
-end
-
-trainthis = true
+trainthis = false
 if trainthis then
-dataPath = "data_1.txt"                                                         
-labelsPath = "labels_1.txt"                                                     
-dataset={}                                                                      
-dataset = get_data_and_labelsMULTILABEL(dataPath,labelsPath)
 
-setmetatable(dataset, 
-				{__index = function(t, i) 
-				return {t.data[i], t.label[i]} 
-				end}
-			);
-		dataset.data = dataset.data:double() -- convert the data from a ByteTensor to
-		-- a DoubleTensor.
+  -- Load the data
+  dataPath = "data_1.txt"
+  labelsPath = "labels_1.txt"
+  dataset={}
+  dataset = get_data_and_labelsNEW(dataPath,labelsPath)
 
-function dataset:size() 
-    return self.data:size(1) 
+  mlp = nn.Sequential(); -- make a multi-layer perceptron
+  inputs = 169; outputs = 6; HUs = 45; -- parameters
+  mlp:add(nn.Linear(inputs, HUs))
+  mlp:add(nn.Tanh())
+  mlp:add(nn.Linear(HUs, outputs))
+
+  print('Lenet5\n' .. mlp:__tostring());
+  mlp:zeroGradParameters() -- zero the internal gradient buffers of the network
+  -- (will come to this later)
+
+  criterion = nn.MSECriterion()
+  trainer = nn.StochasticGradient(mlp, criterion)
+  trainer.learningRate = 0.01
+  trainer.maxIteration = 10 -- just do 5 epochs of training.
+  trainer:train(dataset)
+
 end
 
-for i=1, 2993 do
-dataset.label[i]=dataset.label[i]+1
+trainthis = false
+if trainthis then
+  dataPath = "data_1.txt"
+  labelsPath = "labels_1.txt"
+  dataset={}
+  dataset = get_data_and_labelsMULTILABEL(dataPath,labelsPath)
 
-print(dataset[i][2])
-end
-net = nn.Sequential()
-net:add(nn.Linear(169, 100)) 
-net:add(nn.ReLU())                       -- non-linearity 
-net:add(nn.Linear(100, 64))                   -- 10 is the number of outputs of
-net:add(nn.LogSoftMax())                     -- converts the output to a
---log-probability. Useful for classification problems
+  setmetatable(dataset,
+    {__index = function(t, i)
+        return {t.data[i], t.label[i]}
+        end}
+    );
+    dataset.data = dataset.data:double() -- convert the data from a ByteTensor to
+    -- a DoubleTensor.
 
-print('Lenet5\n' .. net:__tostring());
-net:zeroGradParameters() 
+    function dataset:size()
+      return self.data:size(1)
+    end
 
-criterion = nn.ClassNLLCriterion()
-trainer = nn.StochasticGradient(net, criterion)
-trainer.learningRate = 0.01
-trainer.maxIteration = 5 -- just do 5 epochs of training.
+    for i=1, 2993 do
+      dataset.label[i]=dataset.label[i]+1
+    end
+    net = nn.Sequential()
+    net:add(nn.Linear(169, 100))
+    net:add(nn.ReLU()) -- non-linearity
+    net:add(nn.Linear(100, 64)) -- 10 is the number of outputs of
+    net:add(nn.LogSoftMax()) -- converts the output to a
+    --log-probability. Useful for classification problems
+
+    print('Lenet5\n' .. net:__tostring());
+    net:zeroGradParameters()
+
+    criterion = nn.ClassNLLCriterion()
+    trainer = nn.StochasticGradient(net, criterion)
+    trainer.learningRate = 0.01
+    trainer.maxIteration = 5 -- just do 5 epochs of training.
+
+    trainer:train(dataset)
+
+    torch.save("nnparame.par", net)
+  end
 
 
-trainer:train(dataset)
+  trainthis = true
+  if trainthis then
 
-torch.save("nnparame.par", net)
-end
 
+
+    mlp = nn.Sequential(); -- make a multi-layer perceptron
+    inputs = 169; outputs = 6; HUs = 45; -- parameters
+    mlp:add(nn.Linear(inputs, HUs))
+    mlp:add(nn.Tanh())
+    mlp:add(nn.Linear(HUs, HUs))
+    mlp:add(nn.Tanh())
+    mlp:add(nn.Linear(HUs, HUs))
+    mlp:add(nn.Tanh())
+    mlp:add(nn.Linear(HUs, HUs))
+    mlp:add(nn.Tanh())
+    mlp:add(nn.Linear(HUs, outputs))
+
+    print('Lenet5\n' .. mlp:__tostring());
+    mlp:zeroGradParameters() -- zero the internal gradient buffers of the network
+    -- (will come to this later)
+
+    criterion = nn.MultiLabelMarginCriterion()
+    trainer = nn.StochasticGradient(mlp, criterion)
+    trainer.learningRate = 0.001
+    trainer.maxIteration = 5 -- just do 5 epochs of training.
+
+    -- Load the data
+    dataPath = "../Data/data_2/data_1.txt"
+    labelsPath = "../Data/data_2/labels_1.txt"
+    dataset={}
+    dataset = get_data_and_labelsNEW(dataPath,labelsPath)
+
+    trainer:train(dataset)
+
+    -- Load the data
+    dataPath = "../Data/data_2/data_0.txt"
+    labelsPath = "../Data/data_2/labels_0.txt"
+    dataset={}
+    dataset = get_data_and_labelsNEW(dataPath,labelsPath)
+
+    trainer:train(dataset)
+
+    -- Load the data
+    dataPath = "../Data/data_2/data_2.txt"
+    labelsPath = "../Data/data_2/labels_2.txt"
+    dataset={}
+    dataset = get_data_and_labelsNEW(dataPath,labelsPath)
+
+    trainer:train(dataset)
+
+    torch.save("multilabel.par", mlp)
+
+  end
