34a0e301194b0e3d758eb2c4cd1a3fe17e266fc3
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2016-12-12 23:31:38 -0500

    Adding even More Data

--- utils/record.lua
+++ utils/record.lua
@@ -10,19 +10,19 @@ smb_savestate = savestate.create(1)
 getInputs = Mario.getInputs
 displayBoard = Interface.displayBoard
 totalGameState = {}
-run = 1
+run = 201
 Interface.initializeRun(smb_savestate)
 while true do
     displayBoard()
 	playerStatus = memory.readbyte(0x000E)
     ypos =  memory.readbyte(0x03B8)+16
     gui.drawtext(223, 88, ypos, color)
-
-	if playerStatus ~= 11 and playerStatus ~= 4  and ypos < 220 then
+    gui.drawtext(223, 96, playerStatus, color)
+	if playerStatus ~= 11 and playerStatus ~= 4 and (ypos ~= 256) then
   		gamestate = displayBoard()
 		table.insert(totalGameState, gamestate)
     else 
-        if playerStatus ~= 11 and ypos < 220 then
+        if playerStatus ~= 11 and (ypos == 256) then
         	Fileops.write_full_game(totalGameState, run .. ".txt")
         	run = run + 1
         end
